00c766501807fb793d8abc61e9338c6ed9b047f5
user <pythonakkaunt001@gmail.com>
2023-03-21 16:06:51 +0500

    Start profect

--- keyboards/inline/users_btn.py
+++ keyboards/inline/users_btn.py
@@ -2,7 +2,7 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
 async def currency_btn(data: list):
-    btn = InlineKeyboardMarkup(row_width=5)
+    btn = InlineKeyboardMarkup(row_width=3)
     btn.add(
         *[InlineKeyboardButton(f"{item['title']}", callback_data=f"currency{item['code']}") for item in data]
     )
